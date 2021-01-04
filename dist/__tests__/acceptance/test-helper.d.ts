d91f04aa6ccee982a7412de9f2d6d86d47e93f41
coolmohan <43965929+coolmohan@users.noreply.github.com>
2021-01-04 19:53:43 +0530

    first commit

---
+++ dist/__tests__/acceptance/test-helper.d.ts
@@ -0,0 +1,7 @@
+import { ScaffoldApplication } from '../..';
+import { Client } from '@loopback/testlab';
+export declare function setupApplication(): Promise<AppWithClient>;
+export interface AppWithClient {
+    app: ScaffoldApplication;
+    client: Client;
+}
